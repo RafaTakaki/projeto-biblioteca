72ceb7a5a101cbde12f230044e4fbe450b263f7e
Rafael Takaki <rafalimatakaki@hotmail.com>
2025-11-04 23:19:06 -0300

    fix dependencyinjection

--- Library.Persistence/DependencyInjection/DependencyInjection.cs
+++ Library.Persistence/DependencyInjection/DependencyInjection.cs
@@ -1,10 +1,15 @@
 using Library.Aplication.Services;
+using Library.Domain.Entities;
+using Library.Domain.Enums;
 using Library.Domain.Interface;
 using Library.Persistence.Repositories;
 using Library.Persistence.Settings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 namespace Library.Persistence.DependencyInjection
@@ -35,7 +40,33 @@ namespace Library.Persistence.DependencyInjection
             services.AddScoped<IReservaRepository, ReservaRepository>();
             services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
 
+
+            // Mapear classes para o MongoDB Colocar em um metodo apartado
+            MapearEnum();
+
             return services;
+        }
+
+        private static void MapearEnum()
+        {
+            BsonClassMap.RegisterClassMap<Usuario>(static cm =>
+            {
+                cm.AutoMap();
+                cm.MapMember(c => c.TipoUsuario)
+                    .SetSerializer(new EnumSerializer<TipoUsuario>(BsonType.String));
+            });
+            BsonClassMap.RegisterClassMap<Emprestimo>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapMember(c => c.Status)
+                    .SetSerializer(new EnumSerializer<StatusEmprestimo>(BsonType.String));
+            });
+            BsonClassMap.RegisterClassMap<Reserva>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapMember(c => c.Status)
+                    .SetSerializer(new EnumSerializer<StatusReserva>(BsonType.String));
+            });
         }
     }
 }
