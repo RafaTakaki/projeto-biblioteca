af106ad4c830790c19b3f802c263f72aa1ec52fa
adriana.otte <adriana.otte@outlook.com>
2025-11-15 23:39:16 -0300

    Alterando arquivos de pasta

--- FrontEnd/biblioteca-app/src/app/app.routes.ts
+++ FrontEnd/biblioteca-app/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login-page/login/login.component';
 import { CriarContaComponent } from './components/login-page/criar-conta/criar-conta.component';
 import { RecuperarSenhaComponent } from './components/login-page/recuperar-senha/recuperar-senha.component';
-import { CadastrarLivroComponent } from './components/Pets/cadastrar-livro/cadastrar-livro';
+import { CadastrarLivroComponent } from './components/Livro/cadastrar-livro/cadastrar-livro';
 import { AuthGuard } from './auth.guard';
 import { FrontPageComponent } from './components/front-page/front-page.component';
-import { CriarEmprestimoComponent } from './components/Pets/criar-emprestimos/criar-emprestimo';
+import { CriarEmprestimoComponent } from './components/Livro/criar-emprestimos/criar-emprestimo';
 import { CadastroAgendamentoComponent } from './components/agendamento/cadastro-agendamento/cadastro-agendamento.component';
 import { NotificacoesComponent } from './components/notificacoes/notificacoes.component';
-import { DetalhesPetComponent } from './components/Pets/detalhes-pet/detalhes-pet.component';
+import { DetalhesPetComponent } from './components/Livro/detalhes-pet/detalhes-pet.component';
 
 
 export const routes: Routes = [
