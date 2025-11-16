b60d9920be403136c8c7a8c4a8e15d3de43b440e
adriana.otte <adriana.otte@outlook.com>
2025-11-15 23:39:53 -0300

    Arquivos criados ou alterados durante o build da aplicação em c#

--- Library.Aplication/obj/Debug/net9.0/Library.Aplication.AssemblyInfo.cs
+++ Library.Aplication/obj/Debug/net9.0/Library.Aplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Aplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef04c215ddca8c8fedd56cb0c53614b12da86e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d70aabdb6c4159ddff38521c211bc055b9a140")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Aplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Aplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Library.Persistence/obj/Debug/net9.0/Library.Persistence.AssemblyInfo.cs
+++ Library.Persistence/obj/Debug/net9.0/Library.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef04c215ddca8c8fedd56cb0c53614b12da86e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d70aabdb6c4159ddff38521c211bc055b9a140")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
